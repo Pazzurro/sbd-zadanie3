a7727d0d3c3b277c4093653890de63ce3759e479
Pazzurro <legaldamian133@gmail.com>
2022-11-02 09:54:33 +0100

    zadanie 2

--- zapytania.sql
+++ zapytania.sql
@@ -13,3 +13,24 @@ SELECT * FROM `customers` ORDER BY country ASC;
 SELECT * FROM `customers` ORDER BY country DESC;
 
 SELECT * FROM `customers` ORDER BY country DESC, customer_name ASC;
+
+-------------------------------------------------------------------------
+
+SELECT MIN(price) FROM `products`;
+
+SELECT MAX(price) FROM `products`;
+
+SELECT COUNT(*) FROM `products`;
+
+SELECT COUNT(*) FROM `customers`;
+
+SELECT country, COUNT(*) FROM `customers` GROUP BY country;
+
+SELECT COUNT(*), AVG(price) FROM `products` WHERE category_id = 1;
+
+SELECT SUM(price) FROM `products` WHERE category_id = 2;
+
+SELECT category_id, MAX(price) FROM `products` WHERE category_id = 1 OR category_id = 2 GROUP BY category_id;
+
+
+
