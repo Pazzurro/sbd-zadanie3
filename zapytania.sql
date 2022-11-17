0e9fbbbaa19d57b2fae65ef69cdfc4e854ce56ea
Pazzurro <legaldamian133@gmail.com>
2022-11-17 14:44:40 +0100

    CASE

--- zapytania.sql
+++ zapytania.sql
@@ -1,78 +1,107 @@
 -------------------------------------------------------------------------
 //WHERE_AND_OR_ORDAYBY//
 
-SELECT * FROM `customers` WHERE country = "Germany";
+1. SELECT * FROM `customers` WHERE country = "Germany";
 
-SELECT * FROM `customers` WHERE city = "Berlin";
+2. SELECT * FROM `customers` WHERE city = "Berlin";
 
-SELECT * FROM `customers` WHERE country = "Germany" AND city = "Berlin";
+3. SELECT * FROM `customers` WHERE country = "Germany" AND city = "Berlin";
 
-SELECT * FROM `customers` WHERE (city = "Berlin" OR city = "München") AND country = "Germany";
+4. SELECT * FROM `customers` WHERE (city = "Berlin" OR city = "München") AND country = "Germany";
 
-SELECT * FROM `customers` WHERE NOT country = "Germany" AND NOT country = "USA";
+5. SELECT * FROM `customers` WHERE NOT country = "Germany" AND NOT country = "USA";
 
-SELECT * FROM `customers` ORDER BY country ASC;
+6. SELECT * FROM `customers` ORDER BY country ASC;
 
-SELECT * FROM `customers` ORDER BY country DESC;
+7. SELECT * FROM `customers` ORDER BY country DESC;
 
-SELECT * FROM `customers` ORDER BY country DESC, customer_name ASC;
+8. SELECT * FROM `customers` ORDER BY country DESC, customer_name ASC;
 
 -------------------------------------------------------------------------
 //MIN_MAX_COUNT_AVG_SUM_GROUPBY//
 
-SELECT MIN(price) FROM `products`;
+1. SELECT MIN(price) FROM `products`;
 
-SELECT MAX(price) FROM `products`;
+2. SELECT MAX(price) FROM `products`;
 
-SELECT COUNT(*) FROM `products`;
+3. SELECT COUNT(*) FROM `products`;
 
-SELECT COUNT(*) FROM `customers`;
+4. SELECT COUNT(*) FROM `customers`;
 
-SELECT COUNT(*) FROM `customers` GROUP BY country;
+5. SELECT COUNT(*) FROM `customers` GROUP BY country;
 
-SELECT COUNT(*), AVG(price) FROM `products` WHERE category_id = 1;
+6. SELECT COUNT(*), AVG(price) FROM `products` WHERE category_id = 1;
 
-SELECT SUM(price) FROM `products` WHERE category_id = 2;
+7. SELECT SUM(price) FROM `products` WHERE category_id = 2;
 
-SELECT MAX(price) FROM `products` WHERE category_id = 1 OR category_id = 2 GROUP BY category_id;
+8. SELECT MAX(price) FROM `products` WHERE category_id = 1 OR category_id = 2 GROUP BY category_id;
 
 -------------------------------------------------------------------------
 //BETWEEN_NOT_BETWEEN_IN_NOT_IN_SUBQUERY//
 
-SELECT * FROM `products` WHERE price BETWEEN 10 AND 20;
+1. SELECT * FROM `products` WHERE price BETWEEN 10 AND 20;
 
-SELECT * FROM `products` WHERE price NOT BETWEEN 10 AND 20;
+2. SELECT * FROM `products` WHERE price NOT BETWEEN 10 AND 20;
 
-SELECT * FROM `products` WHERE category_id IN(1, 2, 3);
+3. SELECT * FROM `products` WHERE category_id IN(1, 2, 3);
 
-SELECT * FROM orders WHERE shipper_id IN(1, 2, 3);
+4. SELECT * FROM orders WHERE shipper_id IN(1, 2, 3);
 
-SELECT * FROM `products` WHERE (price BETWEEN 10 AND 15) AND (category_id NOT BETWEEN 1 AND 4);
+5. SELECT * FROM `products` WHERE (price BETWEEN 10 AND 15) AND (category_id NOT BETWEEN 1 AND 4);
 
-SELECT * FROM `orders` WHERE order_date BETWEEN CAST(19960701 AS datetime) AND CAST(19960731 AS datetime);
+6. SELECT * FROM `orders` WHERE order_date BETWEEN CAST(19960701 AS datetime) AND CAST(19960731 AS datetime);
 
-SELECT * FROM `orders` WHERE orders.customer_id IN( SELECT id FROM `customers` WHERE id BETWEEN 77 AND 90);
+7. SELECT * FROM `orders` WHERE orders.customer_id IN( SELECT id FROM `customers` WHERE id BETWEEN 77 AND 90);
 
 -------------------------------------------------------------------------
 //JOIN_INNER_LEFT_RIGT_SELF//
 
-SELECT customers.customer_name, customers.contact_name, customers.address, orders.order_date FROM customers JOIN orders ON customers.id = orders.customer_id;
+1. SELECT customers.customer_name, customers.contact_name, customers.address, orders.order_date FROM customers JOIN orders ON customers.id = orders.customer_id;
 
 
-SELECT customers.customer_name, customers.contact_name, customers.address, COUNT(orders.id) FROM customers JOIN orders ON customers.id = orders.customer_id GROUP BY customers.id;
+2. SELECT customers.customer_name, customers.contact_name, customers.address, COUNT(orders.id) FROM customers JOIN orders ON customers.id = orders.customer_id GROUP BY customers.id;
 
 
-SELECT c.id, c.customer_name, c.contact_name, c.address, o.id FROM customers c RIGHT JOIN orders o ON c.id = o.customer_id WHERE c.id = o.customer_id;
+3. SELECT c.id, c.customer_name, c.contact_name, c.address, o.id FROM customers c RIGHT JOIN orders o ON c.id = o.customer_id WHERE c.id = o.customer_id;
 
 
-SELECT employees.first_name, employees.last_name, orders.id FROM employees RIGHT JOIN orders ON orders.employee_id = employees.id;
+4. SELECT employees.first_name, employees.last_name, orders.id FROM employees RIGHT JOIN orders ON orders.employee_id = employees.id;
 
 
-SELECT C1.customer_name AS name1, C2.customer_name AS name2 FROM customers C1 JOIN customers C2 ON C1.city = C2.city AND C1.id != C2.id;
+5. SELECT C1.customer_name AS name1, C2.customer_name AS name2 FROM customers C1 JOIN customers C2 ON C1.city = C2.city AND C1.id != C2.id;
 
 -------------------------------------------------------------------------
 //LIMIT_OFFSET//
 
-SELECT * FROM orders ORDER BY id DESC LIMIT 10;
+1. SELECT * FROM orders ORDER BY id DESC LIMIT 10;
 
-SELECT * FROM orders LIMIT 10 OFFSET 10;
+2. SELECT * FROM orders LIMIT 10 OFFSET 10;
+
+-------------------------------------------------------------------------
+//HAVING_EXISTS//
+
+1. SELECT country FROM customers GROUP BY country HAVING COUNT(country) > 5;
+
+2. SELECT * FROM customers WHERE EXISTS (SELECT * FROM orders WHERE customers.id = orders.customer_id HAVING COUNT(orders.id) BETWEEN 2 AND 4) ORDER BY customers.id DESC;
+
+3. SELECT employees.id, employees.last_name, employees.first_name FROM orders JOIN employees ON employees.id = orders.employee_id GROUP BY orders.employee_id HAVING COUNT(employee_id) > 10;
+
+4. SELECT employees.id, employees.last_name, employees.first_name FROM employees JOIN orders ON employees.id = orders.employee_id WHERE employees.last_name = "Davolio" OR employees.last_name = "Fuller" GROUP BY orders.employee_id HAVING COUNT(employee_id) > 25;
+
+5. SELECT * FROM suppliers WHERE EXISTS(SELECT * FROM products WHERE suppliers.id = products.supplier_id AND products.price < 20);
+
+6. SELECT * FROM suppliers WHERE EXISTS(SELECT * FROM products WHERE suppliers.id = products.supplier_id AND products.price = 22);
+
+-------------------------------------------------------------------------
+//CASE//
+
+1. SELECT products.name, CASE WHEN orders_details.quantity > 30 THEN "Liczba sztuk większa niż 30" WHEN orders_details.quantity = 30 THEN "Liczba sztuk równa 30" ELSE "Liczba sztuk mniejsza niż 30" END AS liczba_sztuk FROM products JOIN orders_details ON products.id = orders_details.product_id;
+
+2. SELECT * FROM customers ORDER BY (CASE WHEN customers.city IS NULL THEN customers.country ELSE customers.city END);
+
+-------------------------------------------------------------------------
+//ALL//
+
+1. SELECT products.id, products.name, SUM(orders_details.quantity) amount FROM products JOIN orders_details ON products.id = orders_details.product_id GROUP BY products.name ORDER BY amount DESC LIMIT 10;
+
+2. SELECT customers.id, customers.customer_name, COUNT(orders.id) ordersAmount FROM customers JOIN orders ON customers.id = orders.customer_id GROUP BY orders.customer_id ORDER BY `ordersAmount` DESC LIMIT 10;
